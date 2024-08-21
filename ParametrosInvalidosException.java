80734575c16953e8b2f9e5c93dfd5c3e394cccc6
Késia <125037866+aciolik@users.noreply.github.com>
2024-08-20 21:20:08 -0300

    Add files via upload

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,16 @@
+public class ParametrosInvalidosException extends Exception{
+    public static void main(String[] args) {
+
+    }
+
+    static void validarParametro(){
+        System.out.println("Parâmetro Dois deve ser maior que Parametro Um");
+    }
+
+}
+
+
+
+
+
+
